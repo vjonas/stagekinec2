ae420458862b7ae160b0d77cc9669dd7bc605408
MaikoPut94 <maiko.put94@hotmail.com>
2017-04-25 15:49:09 +0200

    useroverview en individual user changes

--- src/app/components/individualuser.component/individualuser.component.ts
+++ src/app/components/individualuser.component/individualuser.component.ts
@@ -1,17 +1,35 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import { slideLeftTransition } from '../../animations/router.animations';
 
 
 @Component({
     selector: 'individualUser',
     templateUrl: 'individualuser.component.html',
-    styleUrls: ['./individualuser.component.css']
+    styleUrls: ['./individualuser.component.css'],
+    animations: [slideLeftTransition()],
+    host: {'[@routerTransition]':''}
 })
 
 export class IndividualUserComponent{
+    teller:number[]=[0,1,2,3,4,5,6,7,8,9,10];
+    isCheckboxChecked:number=0;
+
     constructor(private router: Router){}
 
     goBack(){
         this.router.navigate(["/useroverview"]);
+    }
+
+    changeCheckbox(checkbox:any){
+        console.log(checkbox.srcElement);
+        if(checkbox.currentTarget.childNodes[1].checked==false){
+        checkbox.currentTarget.childNodes[1].checked=true;
+        this.isCheckboxChecked++;
+    }
+    else{
+        checkbox.currentTarget.childNodes[1].checked=false
+        this.isCheckboxChecked--;
+    }
     }
 }
