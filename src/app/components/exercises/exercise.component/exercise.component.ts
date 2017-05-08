70fa7778a4d500c8d4613ccc7bc9fafdcf5fd81d
MaikoPut94 <maiko.put94@hotmail.com>
2017-05-08 08:33:11 +0200

    IDASKK-5 create exercise view design

--- src/app/components/exercises/exercise.component/exercise.component.ts
+++ src/app/components/exercises/exercise.component/exercise.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import{ KinectJoint} from '../../../models/kinectJoint.model'
 import Bezier from 'bezier-js';
 
 @Component({
@@ -8,11 +9,12 @@ import Bezier from 'bezier-js';
     styleUrls: ['./exercise.component.css']
 })
 
-export class ExerciseComponent implements AfterViewInit {
+export class ExerciseComponent implements OnInit, AfterViewInit {
     teller: number[][] = [[0, 2, 3], [1, 3, 4, 5], [2, 6]];
     newExerciseSteps: number[] = [0];
     viewState: boolean = true;
     lineBool: boolean = false;
+    kinectJoints: KinectJoint[] = new Array<KinectJoint>();
     //variables to draw the new line
     public canvas: HTMLCanvasElement;
     public context;
@@ -25,6 +27,34 @@ export class ExerciseComponent implements AfterViewInit {
     private curve: any;
 
     constructor(private router: Router) {
+    }
+
+    ngOnInit(){
+        this.kinectJoints.push(new KinectJoint(0, "Base of the spine"));
+        this.kinectJoints.push(new KinectJoint(1, "Middle of the spine"));
+        this.kinectJoints.push(new KinectJoint(2, "Neck"));
+        this.kinectJoints.push(new KinectJoint(3, "Head"));
+        this.kinectJoints.push(new KinectJoint(4, "Left shoulder"));
+        this.kinectJoints.push(new KinectJoint(5, "Left elbow"));
+        this.kinectJoints.push(new KinectJoint(6, "Left wrist"));
+        this.kinectJoints.push(new KinectJoint(7, "Left hand"));
+        this.kinectJoints.push(new KinectJoint(8, "Right shoulder"));
+        this.kinectJoints.push(new KinectJoint(9, "Right elbow"));
+        this.kinectJoints.push(new KinectJoint(10, "Right wrist"));
+        this.kinectJoints.push(new KinectJoint(11, "Right hand"));
+        this.kinectJoints.push(new KinectJoint(12, "Left hip"));
+        this.kinectJoints.push(new KinectJoint(13, "Left knee"));
+        this.kinectJoints.push(new KinectJoint(14, "Left ankle"));
+        this.kinectJoints.push(new KinectJoint(15, "Left foot"));
+        this.kinectJoints.push(new KinectJoint(16, "Right hip"));
+        this.kinectJoints.push(new KinectJoint(17, "Right knee"));
+        this.kinectJoints.push(new KinectJoint(18, "Right ankle"));
+        this.kinectJoints.push(new KinectJoint(19, "Right foot"));
+        this.kinectJoints.push(new KinectJoint(20, "Spine at the shoulder"));
+        this.kinectJoints.push(new KinectJoint(21, "Tip of the left hand"));
+        this.kinectJoints.push(new KinectJoint(22, "Left thumb"));
+        this.kinectJoints.push(new KinectJoint(23, "Tip of the right hand"));
+        this.kinectJoints.push(new KinectJoint(24, "Right thumb"));
     }
 
     ngAfterViewInit(): void {
---
+++ src/app/models/kinectJoint.model.ts
@@ -0,0 +1,8 @@
+export class KinectJoint
+{
+    constructor(
+        public id: number,
+        public name: string
+    )
+    {}
+}
